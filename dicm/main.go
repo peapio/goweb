a79d28825b74576865fd6633bace10f6d1be4f7f
peapio <aqiiii@yeah.net>
2024-09-06 16:05:44 +0800

    go web init

---
+++ dicm/main.go
@@ -0,0 +1,32 @@
+package main
+
+import(
+    // "log"
+    "net/http"
+    "example/gee"
+)
+
+func main() {
+    r := gee.New()
+    r.GET("/", indexHandler)
+    r.GET("/hello", helloHandler)
+    r.POST("/login", loginHandler)
+
+    r.Run(":9999")
+}
+
+
+func indexHandler(c *gee.Context) {
+    c.HTML(http.StatusOK, "<h1>Hello Gee</h1>")
+}
+
+func helloHandler(c *gee.Context) {
+    c.String(http.StatusOK, "hello %s, you're at %s\n", c.Query("name"), c.Path)
+}
+
+func loginHandler(c *gee.Context) {
+    c.JSON(http.StatusOK, gee.H{
+			"username": c.PostForm("username"),
+			"password": c.PostForm("password"),
+		})
+}
